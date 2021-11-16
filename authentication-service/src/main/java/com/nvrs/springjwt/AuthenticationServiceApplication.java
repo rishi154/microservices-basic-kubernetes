bf08a9d8449eb164df739f85b66ee815786d2807
rishi <rishi154@gmail.com>
2021-11-16 21:59:22 +0530

    Updated for Kubernetes discovery

--- authentication-service/src/main/java/com/nvrs/springjwt/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/nvrs/springjwt/AuthenticationServiceApplication.java
@@ -2,7 +2,9 @@ package com.nvrs.springjwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthenticationServiceApplication {
 
--- customer/src/main/java/com/nvrs/customer/CustomerApplication.java
+++ customer/src/main/java/com/nvrs/customer/CustomerApplication.java
@@ -2,6 +2,9 @@ package com.nvrs.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CustomerApplication {
 
